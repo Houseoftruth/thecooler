0981b26faff1b390b5c4ca0fe97c2999967836ab
ohrha <trigramthree@gmail.com>
2020-05-19 22:20:14 -0700

    'CLEAN-UP-ROUTES'

--- routes/api/beer.js
+++ routes/api/beer.js
@@ -1,33 +1,18 @@
 const express = require('express')
 const app = express();
 const router = express.Router()
-
-const mongodb = require('mongodb')
-const bodyParser = require('body-parser')
 const cache = require('memory-cache')
 const rp = require('request-promise')
-const request = require('request')
 const validationMiddleWare = require('../../zaid_modules/vc')
-
 var DB = require('nosql');
-
 var nosql = DB.load('./database/fridge.nosql');
-console.log(nosql)
-//var db = NoSQL.load('../../database',nosql);
 
-//const validator = require('../../zaid_modules/validator')
-app.use(validationMiddleWare)
-cache.put('foo', {});
-console.log(cache.get('data'));
-var validate = require('validate.js')
 
 var options = {
     uri: 'https://api.punkapi.com/v2/beers?beer_name='
 };
 
 
-    
-//router.use(bodyParser.urlencoded({ 'extended': 'true' })); 
 router.get('/bling', function (req, res) {
 
     res.json("BLING")
@@ -44,8 +29,8 @@ router.post('/',validationMiddleWare, (req, res) => {
 
         if(err){
 
-            res.json({ 
-                        success: false, 
+            res.json({
+                        success: false,
                         message:"There was an issue storing the requests header values to the database..."
                     })
 
@@ -53,18 +38,18 @@ router.post('/',validationMiddleWare, (req, res) => {
 
             options.uri = options.uri + req.body.name
             var cachedData = cache.get(req.body.name)
-        
+
             if (cachedData){
-        
+
                 console.log("Data already exists in cache...Serving cached result...")
                 console.log("Cached data", cachedData)
                 cachedData.rating = req.body.rating;
                 res.json({
-                            success: true, 
+                            success: true,
                             message: req.body.name+" found in the fridge... Enjoy... ;)",
                             data:cachedData
                         })
-                
+
             }else{
                 console.log("Options", options)
                 rp(options)
@@ -75,26 +60,26 @@ router.post('/',validationMiddleWare, (req, res) => {
                             dataObjectLiteral[0].rating = req.body.rating;
 
                             nosql.insert(dataObjectLiteral).callback(function (err) {
-                            
+
                                 if(err){
-                    
+
                                     res.json({success: false, message:"There was an error saving your rating to the database... :(", error:err})
-                                
+
                                 }else{
-                    
+
                                     cache.put(req.body.name,data)
                                     res.json({success: true, message:"Thanks! We've recieved your rating an updated our records...:)", data:data})
-                            
+
                                 }
                             })
-                
-                })  
-            
+
+                })
+
             }
 
 
         }
-   
+
 
    })
 
@@ -104,7 +89,7 @@ router.post('/',validationMiddleWare, (req, res) => {
         nosql.insert(requestObject).callback(function (err) {
             console.log('The user has been created.', err);
         });*/
-     
+
 
   // res.json({success: false})
 })
@@ -119,7 +104,7 @@ router.get('/api.punkapi.com/v2/beers/:beername'
             res.json({ success: true, message: "Beer has been found..", beer: beer })
         }
 
-    })
+})
 
 router.put('/:name',validationMiddleWare, (req, res) => {
 
@@ -130,8 +115,8 @@ router.put('/:name',validationMiddleWare, (req, res) => {
 
         if(err){
 
-            res.json({ 
-                        success: false, 
+            res.json({
+                        success: false,
                         message:"There was an issue storing the requests header values to the database..."
                     })
 
@@ -146,7 +131,7 @@ router.put('/:name',validationMiddleWare, (req, res) => {
                 console.log("Data already exists in cache...Serving cached result...")
                 console.log("Cached data", cachedData)
                 res.json({success: true, message: req.params.name+" found in the fridge... Enjoy... ;)",data:cachedData})
-        
+
             }else{
                // https://api.punkapi.com/v2/beers?beer_name=budweiser
                 console.log("Data does not exist in cache...Retrieving data from uri...")
@@ -165,14 +150,14 @@ router.put('/:name',validationMiddleWare, (req, res) => {
 
                     if(dataLiteral.length==0){
                         res.json({success: false, message:"Beer does not exist at the store...",data:dataLiteral})
- 
+
                     }else{
 
                         console.log("but also here...")
                     var dataLiteral = JSON.parse(data)
 
                     console.log("data",dataLiteral[0])
-                    
+
                     var trimmedResponseObject = {
                         id: dataLiteral[0].id,
                         name: dataLiteral[0].name,
@@ -186,56 +171,24 @@ router.put('/:name',validationMiddleWare, (req, res) => {
 
                     cache.put(req.params.name,trimmedResponseObject)
                     res.json({
-                                success: true, 
-                                message:"Beer does not exist in the fridge...Picked some up @ PunkApi ;)...", 
+                                success: true,
+                                message:"Beer does not exist in the fridge...Picked some up @ PunkApi ;)...",
                                 data:trimmedResponseObject
                             })
-        
 
-                        
+
+
                     }
                 })
-                
+
             }
         }
     })
 
-    
+
 
 })
 
 
 module.exports = router
 
-/*
-module.exports = function (app) {
-
-
-    app.put('https://api.punkapi.com/v2/beers/:beer_name', (req, res) => {
-
-        Date.findOneAndUpdate({ beer_name: req.params.beer_name }, {
-            $set: {
-                three: { state: [0, 0, 0, 0, 0, 0], checkup: [0, 0, 0, 0, 0, 0], discovery: [0, 0, 0, 0, 0, 0] }
-
-
-
-
-            }
-        }, { new: true }, function (err, date) {
-
-            if (err) throw err;
-            if (!date) {
-                res.json({ success: false, message: "Date not found.." })
-            } else {
-                console.log(date)
-                res.json({ success: false, message: "Date not found.." })
-
-            }
-        })
-    })
-
-
-    return app;
-
-}
-*/
